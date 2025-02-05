1f960375c7911b2ba9bbda0bec3fb34d942eb619
Alex Barcelo <alex@betarho.net>
2025-02-05 11:29:14 +0100

    cleaning up, changing active exercise, expanding and briefing things

--- wordcount/solutions/words_active.py
+++ wordcount/solutions/words_active.py
@@ -12,14 +12,24 @@ class Words(DataClayObject):
         or access a distributed storage, or connect to a dataset, retrieve a S3
         object, etc.
         """
-        ...
+        self.raw_text = text
 
     @activemethod
     def split_and_sanitize(self) -> None:
         """Split the text into proper words, store them locally."""
-        ...
+        self.words = []
+        for item in self.raw_text.split():
+            # Remove punctuation signs and make the word lowercase
+            sanitized = item.strip('.,').lower()
+            self.words.append(sanitized)
     
     @activemethod
     def count_words(self) -> dict[str, int]:
         """Count unique words."""
-        ...
+        result = dict()
+        for word in self.words:
+            if word in result:
+                result[word] += 1
+            else:
+                result[word] = 1
+        return result
