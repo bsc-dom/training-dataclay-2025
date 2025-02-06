70a8cb10d8ece02e25d2baf86d1c29113600d307
Alex Barcelo <alex@betarho.net>
2025-02-06 17:45:47 +0100

    making the mqtt broker more reliable (and more simple unintentionally)

--- broker/broker_model/mqttsubs.py
+++ broker/broker_model/mqttsubs.py
@@ -13,32 +13,31 @@ class MqttSubs(DataClayObject, MQTTMixin):
     topic: str
     temperature: str
 
-    @activemethod
     def __init__(self):
         self = self
         self.data = ""
         self.topic = ""
         self.temperature = "NO DATA"
 
-    """ 
-    Here you will have to modify the message_handling function, which is just a placeholder in the 
-    MQTTMixin class.
-    The message has an encoded payload with the data that was sent. (HINT: check the function produce_mqtt_msg 
-    from MQTTMixin)
-        -If the temperature is lower than 5°, we will save the temperature as "freezing" with the (temperature)
-        -If the temperature is between 5° and lower than 17°, we will save the temperature as "cold" 
-         and the (temperature)
-        -If the temperature is higher than 17°, we will save the temperature as "warm" with the (temperature)
-
-        Example: tmp is 7° -> self.temperature = cold(7)
-    """
     @activemethod
     def message_handling(self, client, userdata, msg):
-        from json import loads
+        # To illustrate a message handling, we will be generating human-friendly string as follows:
+        #     -If the temperature is lower than 5°, we will save the temperature as "freezing" with the (temperature)
+        #     -If the temperature is between 5° and lower than 17°, we will save the temperature as "cold" 
+        #      and the (temperature)
+        #     -If the temperature is higher than 17°, we will save the temperature as "warm" with the (temperature)
+        #
+        # Example: If temperature ("tmp") is 7° -> self.temperature = "cold(7)"
+        tmp = float(msg.payload.decode("utf-8"))
 
-        tmp = loads(...)
+        if tmp < 5:
+            self.temperature = "freezing(%.2f)" % tmp
+        elif tmp < 17:
+            self.temperature = "cold(%.2f)" % tmp
+        else:
+            self.temperature = "warm(%.2f)" % tmp
 
-        ...
+        logger.debug("Temperature is %s [tmp=%s]", self.temperature, tmp)        
 
     @activemethod
     def set_topic(self, topic):
